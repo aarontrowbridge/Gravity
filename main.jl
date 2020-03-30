7d320f95904a01c01a5dd8ad1be307b4f681a4b5
AJTrowbridge <aaron.j.trowbridge@gmail.com>
2020-03-30 04:47:10 -0400

    renamed some shit

--- main.jl
+++ main.jl
@@ -19,11 +19,9 @@ end
 
 function anim(io, bs::Vector{Body})
     for b in bs
-        if b.i != 0
-            x, y, z = b.q
-            r = b.r
-            println(io, "c3 $x $y $z $r")
-        end
+        x, y, z = b.q
+        r = b.r
+        println(io, "c3 $x $y $z $r")
     end
     println(io, "T -0.9 0.9")
     println(io, "n = $(length(bs))")
@@ -57,13 +55,14 @@ function main()
         # brute_evolve!(bodies, dt)
         tree_evolve!(bodies, dt, L, θ)
 
+        if star coalesce_star!(bodies) end
         trim!(bodies, L)
 
         # coalesce!(bodies)
-        if star coalesce_star!(bodies) end
+
 
         if itr % 50 == 0
-            if itr % 500 == 0 println(itr) end
+            if itr % 500 == 0 println("itr = $(itr)") end
             anim(io, bodies)
         end
 
